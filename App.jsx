bdc01f4f8a4c181febbd6022c365a79ce7250153
Amit Singh <amitsingh@Amits-MacBook-Pro.local>
2024-05-12 20:12:14 +0530

    add files

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+
+const Application = () => {
+  return (
+    <View>
+      <Text>Hello!!!!!!!!!</Text>
+      <Text>Start Facebook Login</Text>
+    </View>
+  )
+}
+
+export default Application
